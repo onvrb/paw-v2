11f932a3862f4c9b2405f2ae30a5eacaca3ceeb6
onlinecrack <rodrigofernandesribeiro14@gmail.com>
2021-05-02 23:31:52 +0100

    added date/hour/relationship with locationname to events

--- models/event.js
+++ models/event.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
+var Location = require('./location').schema;
 
 var EventSchema = new mongoose.Schema({
     name: { type: String, unique: true},
     description: String,
+    locationname: Location,
     poster: String,
     nTicketsAvailable: Number,
     nTicketsPurchased: Number,
-    price: Number
+    price: Number,
+    date: String,
+    hour: String
 });
 
 module.exports = mongoose.model('Event', EventSchema);
