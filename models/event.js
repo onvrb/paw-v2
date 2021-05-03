6c8e0b7397b15527730350345e48c87671e99b81
onvrb <23731360+onvrb@users.noreply.github.com>
2021-05-03 01:36:40 +0100

    Revert "Revert "update event schema""
    This reverts commit e99b12f1865611c618a1cb14f4f8e0e0b4eb58d4.

--- models/event.js
+++ models/event.js
@@ -4,13 +4,13 @@ var Location = require('./location').schema;
 var EventSchema = new mongoose.Schema({
     name: { type: String, unique: true},
     description: String,
-    locationname: Location,
+    date: String, // tem de ser string, caso seja Date ao editar vem vazio
+    time: String,
+    locationName: String,
     poster: String,
     nTicketsAvailable: Number,
     nTicketsPurchased: Number,
-    price: Number,
-    date: String,
-    hour: String
+    price: Number
 });
 
 module.exports = mongoose.model('Event', EventSchema);
