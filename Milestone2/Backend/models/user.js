64bf0dd2e0c62245f950ad9892e107d0405bb638
Carolina <8180091@estg.ipp.pt>
2021-05-31 23:03:30 +0100

    user model rework

--- Milestone2/Backend/models/user.js
+++ Milestone2/Backend/models/user.js
@@ -2,12 +2,12 @@ var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  name: String,
-  password: String,
-  type: { type: Schema.Types.ObjectId, ref: 'UserType' }, //para importar a referencia de userType
-  covid: Boolean,
-  banned: Boolean,
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  type: { type: Schema.Types.ObjectId, ref: 'UserType', required: true }, //para importar a referencia de userType
+  covid: { type: Boolean, default: false },
+  banned: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("User", UserSchema);
