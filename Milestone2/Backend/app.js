c987a6a755ea47e08cb2c8657a069e038dac92a9
Carolina <8180091@estg.ipp.pt>
2021-06-01 00:21:52 +0100

    added swagger and cors to project

--- Milestone2/Backend/app.js
+++ Milestone2/Backend/app.js
@@ -4,6 +4,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+const express = require('express');
+var cors = require('cors');
+const app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var databaseUtilities = require("./utilities/database");
 var indexRouter = require("./routes/index");
@@ -34,6 +40,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
