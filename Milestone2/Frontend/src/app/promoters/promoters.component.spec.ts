2c1e54876ce601588ee72d1bbe75ff1f9372278d
Carolina <8180091@estg.ipp.pt>
2021-06-10 17:59:27 +0100

    added more components

---
+++ Milestone2/Frontend/src/app/promoters/promoters.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PromotersComponent } from './promoters.component';
+
+describe('PromotersComponent', () => {
+  let component: PromotersComponent;
+  let fixture: ComponentFixture<PromotersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PromotersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PromotersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
