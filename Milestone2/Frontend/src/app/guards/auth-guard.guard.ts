11f3d9b0aa475d3f815e3e3a7d36ac4a5ce0d2cb
Carolina <8180091@estg.ipp.pt>
2021-06-10 23:51:32 +0100

    tentativa guard

--- Milestone2/Frontend/src/app/guards/auth-guard.guard.ts
+++ Milestone2/Frontend/src/app/guards/auth-guard.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
+
+  constructor (private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (localStorage.getItem('currentUser') ) {
+        return true;
+      }
+
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+
+      return false;
   }
   
 }
