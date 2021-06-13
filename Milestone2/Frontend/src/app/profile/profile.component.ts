4f0f36f3f64c78dab0735dc42b471c90f7eb1e14
onvrb <23731360+onvrb@users.noreply.github.com>
2021-06-13 19:55:37 +0100

    update profile component

--- Milestone2/Frontend/src/app/profile/profile.component.ts
+++ Milestone2/Frontend/src/app/profile/profile.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  id: string;
   name: string;
   email: string;
+  covid: string;
   password: string;
+  type: string;
   constructor() { 
     let user = JSON.parse(localStorage.getItem('currentUser') || 'N/A');
+    this.id = user._id;
     this.name = user.name;
     this.email = user.email;
+    this.covid = user.covid;
     this.password = user.password;
+    this.type = user.type.type;
 }
 
   ngOnInit(): void {
